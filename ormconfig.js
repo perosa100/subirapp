26a8b311212c0bb12f6d200cab9e2d77802d96e0
Patrick perosa <patrickperosapp@gmail.com>
2021-04-20 15:01:05 -0400

    mpvp
    Signed-off-by: Patrick perosa <patrickperosapp@gmail.com>

--- ormconfig.js
+++ ormconfig.js
@@ -1,7 +1,7 @@
 console.log('process.env.DATABASEE_URL :>> ', process.env.DATABASE_URL);
 module.exports = {
   type: 'postgres',
-  url: process.env.DATABASE_URL,
+  url: process.env.HEROKU_POSTGRESQL_AQUA_URL,
   entities: ['dist/models/**/*.js'],
   migrations: ['dist/database/migrations/**/*.js'],
   cli: {
